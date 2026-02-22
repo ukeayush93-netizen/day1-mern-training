5f13633c043c408fca58e9f83d0fa25d7f069fd4
ukeayush93-netizen <ukeayush93@gmail.com>
2026-02-22 14:16:27 +0530

    Added variables example

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-console.log("My Name is Ayush Uke");
-console.log("I am learning MERN Stack");
-console.log("Today is Day 1 Practical");
+//console.log("My Name is Ayush Uke");
+//console.log("I am learning MERN Stack");
+//console.log("Today is Day 1 Practical");
+
+const name = "Ayush Uke";
+const course = "MERN Stack";
+
+console.log("Hello, My Name is " + name);
+console.log("I am enrolled in " + course);
